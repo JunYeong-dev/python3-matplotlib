afc0025203f999aec063d7dc92ae678b732f4b0b
Jun-cloud <kjy8827@gmail.com>
2020-05-25 21:40:54 +0900

    simple straight line graph

---
+++ simple_straight_line_graph.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+# Matplotlib
+# 다양한 데이터를 시각화할 수 있도록 도와주는 라이브러리
+# 간단한 데이터 분석에서부터 인공지능 모델의 시각화까지 활용도가 매우 높음
+
+x = [1, 2, 3]
+y = [1, 2, 3]
+plt.plot(x, y)
+plt.title("Straight Line Graph")
+plt.xlabel("X")
+plt.xlabel("Y")
+plt.show()
